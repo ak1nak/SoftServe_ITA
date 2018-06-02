cd9c331e50d7d1b0b992eacb375cd2016d6a0918
ak1nak <sergeycerb@gmail.com>
2018-06-02 19:09:54 +0300

    commit

--- news_portal/Observer_pattern.js
+++ news_portal/Observer_pattern.js
@@ -1,30 +1,44 @@
-"use strict";
+"use strict"
 
-class News {
-    constructor () {
-        this.users = []
+class News{
+    constructor() {
+        this._users = [];
     }
-
-    subscribe (fn) {
-        this.users.push(fn)
+    subscribe(cb) {
+        this._users.push(cb);
     }
-
-    unsubscribe (fn) {
-        this.users = this.users.filter(subscriber => subscriber !== fn)
+    subscribeAll(...rest) {
+        this._users = this._users.concat(rest);
     }
+    unsubscribe(cb) {
+        this._users = this._users.filter(subscriber => subscriber !== cb);
 
-    sendNews (data) {
-        this.users.forEach(subscriber => subscriber(data))
+        //*через splice*
+        //this._cbIndex = this._users.indexOf(cb);
+        //this._users.splice(this._cbIndex, 1);
+
+        //*быдло-способ :)*
+        //this._cbIndex = this._users.indexOf(cb);
+        //delete this._users[this._cbIndex];
+    }
+    sendNews(data) {
+        this._users.forEach(subscriber => subscriber(data));
+    }
+    once(cb) {
+    
+    }
+    subsList() {
+        console.log(this._users);
     }
 }
 
-class User {
-    constructor (name, likesGenre) {
+class User{
+    constructor(name, likesGenre) {
         this.name = name;
         this.likesGenre = likesGenre;
-        this.readNews = (data) => {console.log (data, `${this.name} got it!`);}
+        this.readNews = this.readNews.bind(this);
     }
-
+    readNews(data) {console.log (data, ` ${this.name} got it!`)}
 }
 
 const gameNews = new News();
@@ -32,14 +46,18 @@ const gameNews = new News();
 const gamer1 = new User('Peter', 'action');
 const gamer2 = new User('Vasya', 'RPG');
 const gamer3 = new User('Lena', 'strategy');
+const gamer4 = new User('Sam', 'sims');
+const gamer5 = new User('Hanna', 'MMORPG');
 
 gameNews.subscribe(gamer1.readNews);
 gameNews.subscribe(gamer2.readNews);
-gameNews.subscribe(gamer3.readNews);
 
+gameNews.subscribeAll(gamer3.readNews, gamer4.readNews, gamer5.readNews);
 
 gameNews.sendNews('Half-life 3 is coming!');
 
+gameNews.subsList();
 gameNews.unsubscribe(gamer1.readNews);
+gameNews.subsList();
 
-gameNews.sendNews('Half-life 3 is not coming! LUL');
+gameNews.sendNews('Half-life 3 is not coming! LUL. ');
