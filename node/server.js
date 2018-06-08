c90c408398776c6640faf19d35a2ac2f9b9f0479
ak1nak <sergeycerb@gmail.com>
2018-06-08 11:58:19 +0300

    commit

--- node/server.js
+++ node/server.js
@@ -6,7 +6,7 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, resp) => {
-    fs.readFile('./resp/response.txt', (err, data) => {console.log(data)});
+    fs.readFile('./index.html', 'utf8', (err, data) => {console.log(data)});
     resp.end('done');
 }).listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
