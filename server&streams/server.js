140f541153648f4f317209d565176e4377e63ad2
ak1nak <sergeycerb@gmail.com>
2018-06-11 18:46:44 +0300

    commit

--- server&streams/server.js
+++ server&streams/server.js
@@ -1,25 +1,30 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const qs = require('querystring');
 
 const server = http.createServer((req, res) => {
 
     if (req.method === "GET") {
         res.writeHead(200, { "Content-Type": "text/html" });
         fs.createReadStream(path.join(__dirname, "index.html"), "utf8").pipe(res);
-    } else if (req.method === "POST") {
-        
+    } else if (req.method === "POST") {        
         let body = "";
+        let cws = fs.createWriteStream(path.join(__dirname, "form.txt"));
 
         req.on("data", (chunk) => {
-            fs.appendFile(path.join(__dirname, "form.txt"), chunk + '\r\n', (err) => { if (err) throw err; });
-            body += chunk.toString();
+            cws.write(chunk, (err) => { if (err) throw err; } );
+            body += chunk;
         });
 
         req.on("end", () => {
             res.writeHead(200, { "Content-Type": "text/html" });
-            res.end((body.split('&').join('<br>')));
+            cws.end();
+            res.end(JSON.stringify(qs.parse(body)));            
         });
+    } else {
+        res.writeHead(401, { "Content-Type": "text/html" });
+        res.end('Bad Request')
     }
 
 }).listen(3000, () => {console.log("Server is working: ")});
